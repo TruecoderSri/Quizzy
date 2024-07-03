afe9899881b27693fbaf900fb4205214ed515924
VaibhavArora314 <vaibhav.afw@gmail.com>
2024-07-03 19:12:12 +0530

    move server logic to api

--- backend/api/index.js
+++ backend/api/index.js
@@ -1,3 +1,19 @@
-import { app } from "../server.js"
+import quizRoutes from "../routes/quizRoutes.js";
+import profileRoutes from "../routes/profileRoutes.js";
+import cors from "cors";
+
+config();
+
+const app = express();
+
+app.use(cors());
+connectDB();
+
+app.use(json());
+
+app.use("/api/auth", authRoutes);
+app.use("/api/questions", questionRoutes);
+app.use("/api/quizzes", quizRoutes);
+app.use("/api/users", profileRoutes);
 
 export default app;
