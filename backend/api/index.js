5bb2faedf02172e6427ece18ed344d617b0cd3b8
VaibhavArora314 <vaibhav.afw@gmail.com>
2024-07-03 19:36:41 +0530

    change backend logic

--- backend/api/index.js
+++ backend/api/index.js
@@ -1,3 +1,3 @@
-import { app } from "../server";
+import { app } from "../app.js";
 
 export default app;
--- backend/server.js
+++ backend/server.js
@@ -1,32 +1,7 @@
-import express, { json } from "express";
-import connectDB from "./config/db.js";
-import { config } from "dotenv";
-import authRoutes from "./routes/authRoutes.js";
-import questionRoutes from "./routes/questionRoutes.js";
-import quizRoutes from "./routes/quizRoutes.js";
-import profileRoutes from "./routes/profileRoutes.js";
-import cors from "cors";
+import app from "./app";
 
-config();
+const PORT = 5000;
 
-const app = express();
-
-app.use(cors());
-connectDB();
-
-app.use(json());
-
-app.use("/api/auth", authRoutes);
-app.use("/api/questions", questionRoutes);
-app.use("/api/quizzes", quizRoutes);
-app.use("/api/users", profileRoutes);
-
-// const PORT = 5000;
-
-// app.listen(PORT, () => {
-//   console.log(`Server started on port ${PORT}`);
-// });
-
-export {
-    app
-}
+app.listen(PORT, () => {
+  console.log(`Server started on port ${PORT}`);
+});
