5dc22bf7138c55001e93f77b2a999a817723c97d
VaibhavArora314 <vaibhav.afw@gmail.com>
2024-07-03 19:34:24 +0530

    import change

--- backend/api/index.js
+++ backend/api/index.js
@@ -1,19 +1,3 @@
-import quizRoutes from "../routes/quizRoutes.js";
-import profileRoutes from "../routes/profileRoutes.js";
-import cors from "cors";
-
-config();
-
-const app = express();
-
-app.use(cors());
-connectDB();
-
-app.use(json());
-
-app.use("/api/auth", authRoutes);
-app.use("/api/questions", questionRoutes);
-app.use("/api/quizzes", quizRoutes);
-app.use("/api/users", profileRoutes);
+import { app } from "../server";
 
 export default app;
