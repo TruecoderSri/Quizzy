9c316cf6de168bc744a994508ae1c52b4b996178
Srijan <100436695+TruecoderSri@users.noreply.github.com>
2024-07-03 17:13:22 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,36 +1,11 @@
-// import express, { json } from "express";
-// import connectDB from "./config/db.js";
-// import { config } from "dotenv";
-// import authRoutes from "./routes/authRoutes.js";
-// import questionRoutes from "./routes/questionRoutes.js";
-// import quizRoutes from "./routes/quizRoutes.js";
-// import profileRoutes from "./routes/profileRoutes.js";
-// import cors from "cors";
-
-// config();
-
-// const app = express();
-
-// app.use(cors());
-// connectDB();
-
-// app.use(json());
-
-// app.use("/api/auth", authRoutes);
-// app.use("/api/questions", questionRoutes);
-// app.use("/api/quizzes", quizRoutes);
-// app.use("/api/users", profileRoutes);
-
-// const PORT = 5000;
-
-// app.listen(PORT, () => {
-//   console.log(`Server started on port ${PORT}`);
-// });
-
-const express = require('express');
-const connectDB = require('./config/db.js');
-const { config } = require('dotenv');
-const cors = require('cors');
+import express, { json } from "express";
+import connectDB from "./config/db.js";
+import { config } from "dotenv";
+import authRoutes from "./routes/authRoutes.js";
+import questionRoutes from "./routes/questionRoutes.js";
+import quizRoutes from "./routes/quizRoutes.js";
+import profileRoutes from "./routes/profileRoutes.js";
+import cors from "cors";
 
 config();
 
@@ -39,7 +14,15 @@ const app = express();
 app.use(cors());
 connectDB();
 
-app.use(express.json());
+app.use(json());
 
-module.exports = app;
+app.use("/api/auth", authRoutes);
+app.use("/api/questions", questionRoutes);
+app.use("/api/quizzes", quizRoutes);
+app.use("/api/users", profileRoutes);
 
+const PORT = 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server started on port ${PORT}`);
+});
